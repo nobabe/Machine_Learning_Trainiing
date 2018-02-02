5e09e1bfe481f6878ba5962f503f4ec51fdeacd9
Zheng Yang <nobabe@163.com>
2018-02-02 22:47:57 +0800

    create .gitignore file

---
+++ tensorflow/gd.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import numpy as np 
+
+#create data
+x_data = np.random.rand(200).astype(np.float32)
+y_data = x_data	* 0.1 + 0.3
+
+#create tensorflow structure start
+
+Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+biases = tf.Variable(tf.zeros([1]))
+
+y = Weights * x_data + biases
+
+
+loss = tf.reduce_mean(tf.square(y - y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.1)  
+train = optimizer.minimize(loss)
+
+#init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
+#create tensorflow structure end
+
+sess = tf.Session()
+sess.run(init)   # very importtant
+
+
+for step in range(10201):
+	sess.run(train)
+	if step % 20 == 0:
+		print(step, sess.run(Weights), sess.run(biases))
+
+
+
